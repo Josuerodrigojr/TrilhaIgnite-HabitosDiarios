5592e57e48c57f49609945ef3e32ddad351b26da
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2023-01-23 18:14:15 -0300

    Finalizando aula 5

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -13,7 +13,7 @@ return(
 <img src={logoImage} alt=""/>
 
 <Dialog.Root>
-<Dialog.Trigger  type="button" className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-400 '>
+<Dialog.Trigger  type="button" className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-400 transition-colors focus:outline-none focus:rind-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background'>
   <Plus size={20} className='text-violent-500'/>
   
   Novo hábito
@@ -23,7 +23,7 @@ return(
   <Dialog.Portal>
     <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0'/>
     <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-      <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:tex-zinc-200'>
+      <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:tex-zinc-200 rounded-lg focus:outline-none focus:rind-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zin-900'>
         <X size={24} arial-label="Fechar"/>
       </Dialog.Close>
       <Dialog.Title className='text-3xl leading-tight font-extrabold'>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -73,11 +73,11 @@ weekDays,
         return(
           <CheckBox.Root 
           key={weekDay} 
-          className='flex items-center gap-3 group'
+          className='flex items-center gap-3 group focus-outline-none'
           checked={weekDays.includes(index)}
           onCheckedChange={()=>{handleToggleWeekDay(index)}}
           >
-          <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+          <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:outline-none group-focus:rind-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900'>
             <CheckBox.Indicator>
               <Check size={20} className="text-white" />
             </CheckBox.Indicator>
@@ -94,7 +94,7 @@ weekDays,
      
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:rind-2 focus:ring-green-700 focus:ring-offset-2 focus:ring-offset-zing-600">
         <Check size={20} weight="bold"/>
         Confirmar
       </button>
